dca9834829520b55ca0530ee5815ba4d74bdc4f3
Paul Torres <pntorres1542@gmail.com>
2020-09-27 10:54:02 -0400

    Fixed bug in helper functions

--- Helpers.py
+++ Helpers.py
@@ -1,75 +1,77 @@
 import pandas as pd
 import numpy as np
+
+
 def nyc_locator(df):
     df = df[(df['state'].eq('NY'))]
     df = df[(df['county'].eq('Bronx County'))
-                      | (df['county'].eq('Kings County'))
-                      | (df['county'].eq('Queens County'))
-                      | (df['county'].eq('New York County'))]
+            | (df['county'].eq('Kings County'))
+            | (df['county'].eq('Queens County'))
+            | (df['county'].eq('New York County'))]
     return df
 
 
-def pipe_full(df,ppl_list,house_list, yr):
+def boro_split(df):
+    bx = df[(df['county'].eq('Bronx County'))]
+    bk = df[(df['county'].eq('Kings County'))]
+    qn = df[(df['county'].eq('Queens County'))]
+    mh = df[(df['county'].eq('New York County'))]
+
+    return bk, bx, mh, qn
+
+
+def pipe_full00(df, ppl_list, house_list):
     for x in df.columns.values:
         if x in ppl_list:
-            df[x + '%'] = 100 * df[x] / df['POP' + str(yr)]
+            df[x + '%'] = 100 * df[x] / df['POP00']
         elif x in house_list:
-            df[x + '%'] = 100 * df[x] / df['HU' + str(yr)]
-    # the percentage of non
-    df['NONWHT'+str(yr)+'%'] = 100*(df.iloc[:,10:14].sum(1)/df['POP'+str(yr)+'%])
-    df.drop(columns = ['HU'+str(yr)+'%'], axis = 1, inplace = True)
-    df.drop(df.columns[9:51],axis=1,inplace=True)
-    df.drop(df.columns[4:8],axis=1,inplace=True)
+            df[x + '%'] = 100 * df[x] / df['HU00']
+
+    df['NONWHT00%'] = 100 * (df.iloc[:, [10,11,13]].sum(1) / df['POP00'])
+    df.drop(columns=['HU00%'], axis=1, inplace=True)
+    df.drop(df.columns[9:51], axis=1, inplace=True)
+    df.drop(df.columns[4:8], axis=1, inplace=True)
 
     return df
 
 
-def separator(df,yr_start,yr_end):
+def pipe_full10(df, ppl, house):
+    for x in df.columns.values:
+        if x in ppl:
+            df[x + '%'] = 100 * (df[x] / df['POP10'])
+        elif x in house:
+            df[x + '%'] = 100 * (df[x] / df['HU10'])
+
+    df['NONWHT10%'] = 100 * (df.iloc[:, [6,7,9]].sum(1) / df['POP10'])
+    df.drop(df.columns[5:47], axis=1, inplace=True)
+    return df
+
+
+def separator(df, yr_start, yr_end):
     lst_start = [x for x in df.columns.values if str(yr_start) in x]
     lst_end = [x for x in df.columns.values if str(yr_end) in x]
     lst = list(zip(lst_start, lst_end))
     return lst
 
-def pc_change(df,yr_start,yr_end ):
-    def separator(df,yr_start, yr_end):
-        lst_start = [x for x in df.columns.values if str(yr_start) in x]
-        lst_end = [x for x in df.columns.values if str(yr_end) in x]
-        lst = list(zip(lst_start, lst_end))
-        return lst
 
+def pc_change(df, lst):
     for i, x in lst:
         try:
-            df[i + '_' + x] = 100 * ((df[i] - df[x]) / (df[x]))
+            df[x + '_' + i] = 100 * ((df[x] - df[i]) / (df[i]))
         except ZeroDivisionError:
             0
     return df
 
 
-def pipe_sample(df,ppl,hou,hh,yr):
+def pipe_sample00(df, ppl, hou, hh):
     for x in df.columns.values:
         if x in ppl:
-            df[x + '%'] = 100 * df[x] / df['POP'+str(yr)+'SF3']
+            df[x + '%'] = 100 * df[x] / df['POP00SF3']
         elif x in hou:
-            df[x + '%'] = 100 * df[x] / df['HU'+str(yr)]
+            df[x + '%'] = 100 * df[x] / df['HU00SP']
         elif x in hh:
-            df[x + '%'] = 100 * df[x] / df['HH'+str(yr)]
-    # the percentage of non
-    df['NONWHT_POV'+str(yr)+'%'] = 100*(df.iloc[:,[49,51,53,55]].sum(1)/df['POP'+str(yr)+'SF3'])
+            df[x + '%'] = 100 * df[x] / df['HH00']
 
+    df['NONWHT_POV00%'] = 100*(df.iloc[:,[49,51,53,55]].sum(1)/df['POP00SF3'])
+    
     return df
-
-
-
-def pop_pipe(df,ppl,hou,hh):
-    for x in df.columns.values:
-        if x in ppl:
-            df[x + '%'] = 100 * df[x] / df['POP12SF3']
-        elif x in hou:
-            df[x + '%'] = 100 * df[x] / df['HU12']
-        elif x in hh:
-            df[x + '%'] = 100 * df[x] / df['HH12']
-    # the percentage of non-white people in poverty
-    df['NONWHT_POV12%'] = 100*(df.iloc[:,[32,34,38]].sum(1)/df['POP12SF3'])
-
-
-
